7959e2e5486bee393543a303db59c3e613fdfed1
btk <114290520+0xbtk@users.noreply.github.com>
2023-10-10 13:12:48 +0100

    Update Deployer.sol

--- test/Deployer.sol
+++ test/Deployer.sol
@@ -2,6 +2,6 @@
 
 pragma solidity ^0.8.2;
 
-import "@openzeppelin//token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/token/ERC1155/utils/ERC1155Holder.sol";
 
 contract Deployer is ERC1155Holder {}
